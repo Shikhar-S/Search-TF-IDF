fc45f9b51a39e12bc0c733a71c6e202d7c88750e
Shikhar-S <shikhar.coder@gmail.com>
2016-05-29 19:47:04 +0530

    added nltk for stopwords.

--- Querying.py
+++ Querying.py
@@ -1,13 +1,16 @@
 __author__ = 'Shikhar'
 import MySQLdb
-import math
+
+
+
 class Querying:
-    def __init__(self,usID='shikhar', pswd='password', dbase='indexing_project', hst='localhost'):
-        self.query=''
+    def __init__(self, usID='shikhar', pswd='password', dbase='indexing_project', hst='localhost', n=0):
+        self.query = ''
         self.userID = usID
         self.password = pswd
         self.database = dbase
         self.host = hst
+        self.N = n
         try:
             self.db = MySQLdb.connect(self.host, self.userID, self.password, self.database)
         except:
@@ -17,7 +20,12 @@ class Querying:
         self.db.close()
 
     def getQuery(self):
-        self.query=raw_input('Enter Query')
+        self.query = raw_input('Enter Query')
 
-    def getScore(self):
-
+    def getWeight(self, word, doc_num):
+        cursor = self.db.cursor()
+        cursor.execute("SELECT IDF_VALUE FROM IDF WHERE WORD='%s'" % word)
+        idf = cursor[0][0]
+        tf_retrieval = "SELECT TF_VALUE FROM DOC%d WHERE WORD='%s'" % (doc_num, word)
+        cursor.execute(tf_retrieval)
+        return cursor[0][0] * idf
