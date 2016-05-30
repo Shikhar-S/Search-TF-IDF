b4f4f2a888ee0f7765c4bd6d5d82fda13b9716ac
Shikhar-S <shikhar.coder@gmail.com>
2016-05-30 18:07:39 +0530

    Works faster now Changed Commit calls

--- Index_individual.py
+++ Index_individual.py
@@ -8,7 +8,8 @@ from porterStemmer import PorterStemmer
 from collections import defaultdict
 
 stemmer = PorterStemmer()
-warnings.filterwarnings('ignore','Unknown table')
+warnings.filterwarnings('ignore', 'Unknown table')
+
 
 class Indexing:
     def __init__(self, stpwrdfile, usID='shikhar', pswd='password', dbase='indexing_project', hst='localhost'):
@@ -18,7 +19,7 @@ class Indexing:
         self.host = hst
         self.stopWordFile = stpwrdfile
         self.N = 0
-        self.stopWordSet=set()
+        self.stopWordSet = set()
         self.query_insert = ''
         try:
             self.db = MySQLdb.connect(self.host, self.userID, self.password, self.database)
@@ -28,10 +29,20 @@ class Indexing:
     def __del__(self):
         self.db.close()
 
-    def setUpIDF(self):
-        maxlenWord=20
+    def setUpFileInfo(self):
+        cursor = self.db.cursor()
+        cursor.execute('DROP TABLE IF EXISTS FILES')
+        cursor.execute('CREATE TABLE FILES(FILE_ID INT NOT NULL,LINK VARCHAR(150))')
         try:
-            cursor=self.db.cursor()
+            self.db.commit()
+        except Exception, e:
+            self.db.rollback()
+            print e
+
+    def setUpIDF(self):
+        maxlenWord = 20
+        try:
+            cursor = self.db.cursor()
             cursor.execute("DROP TABLE IF EXISTS IDF")
             idf = 'CREATE TABLE IDF(WORD CHAR(%d) NOT NULL,' % maxlenWord
             idf += 'IDF_VALUE FLOAT)'
@@ -42,13 +53,13 @@ class Indexing:
             print e
             print 'Error creating idf database'
 
-    def setUpTF(self,doc_num):
+    def setUpTF(self, doc_num):
         # n is the number of files to be indexed
         # set up TF and IDF databases
         maxlenWord = 20
         try:
             cursor = self.db.cursor()
-            cursor.execute('DROP TABLE IF EXISTS DOC'+str(doc_num))
+            cursor.execute('DROP TABLE IF EXISTS DOC' + str(doc_num))
             tf = 'CREATE TABLE DOC%d(WORD CHAR(%d) NOT NULL,' % (doc_num, maxlenWord)
             tf += 'TF_VALUE FLOAT)'
             cursor.execute(tf)
@@ -56,10 +67,35 @@ class Indexing:
         except Exception, e:
             self.db.rollback()
             print e
-            print 'Error creating tf database ',doc_num
+            print 'Error creating tf database ', doc_num
 
-    def removeStopWords(self, wordlist):
-        return [x for x in wordlist if x not in self.stopWordSet]
+    def getStopwords(self):
+        with open(self.stopWordFile) as F_Stop:
+            self.stopWordSet = set(F_Stop.read().split(' '))
+
+    def IDF_insert(self):
+        cursor = self.db.cursor()
+        # for every word get freq of no of tables it is part of store it in idf table then
+        for i in xrange(1, self.N + 1):
+            insertion_cursor = self.db.cursor()
+            traversal_cursor = self.db.cursor()
+            traversal_cursor.execute("SELECT WORD FROM DOC%d" % i)
+            for current_word in traversal_cursor:
+                c = 0
+                for j in xrange(1, self.N + 1):
+                    inner_query = "SELECT * FROM DOC%d WHERE WORD='%s'" % (j, current_word[0])
+                    temp_cursor = self.db.cursor()
+                    temp_cursor.execute(inner_query)
+                    if temp_cursor.rowcount > 0:
+                        c += 1
+                c = math.log(self.N / float(c))
+                if c != 0:
+                    insertion_cursor.execute("INSERT INTO IDF(WORD,IDF_VALUE) VALUES('%s',%f)" % (current_word[0], c))
+        try:
+            self.db.commit()
+        except Exception, e:
+            self.db.rollback()
+            print e
 
 
     def TF_insert(self, fid, word, count_total, word_freq):
@@ -74,39 +110,9 @@ class Indexing:
             else:
                 query_update = 'UPDATE TF SET DOC' + str(fid) + ('=%f ' % value) + ("WHERE WORD='%s'" % word)
                 cursor.execute(query_update)
-            self.db.commit()
         except Exception, e:
             print e
-            self.db.rollback()
             # print 'Error entering value to data base for %s' % word
-    def getStopwords(self):
-        with open(self.stopWordFile) as F_Stop:
-            self.stopWordSet = set(F_Stop.read().split(' '))
-
-    def IDF_insert(self):
-        cursor = self.db.cursor()
-        #for every word get freq of no of tables it is part of store it in idf table then
-        for i in xrange(1,self.N+1):
-            insertion_cursor=self.db.cursor()
-            traversal_cursor=self.db.cursor()
-            traversal_cursor.execute("SELECT WORD FROM DOC%d" % i)
-            for current_word in traversal_cursor:
-                c=0
-                for j in xrange(1,self.N+1):
-                    inner_query="SELECT * FROM DOC%d WHERE WORD='%s'" % (j,current_word[0])
-                    temp_cursor=self.db.cursor()
-                    temp_cursor.execute(inner_query)
-                    if temp_cursor.rowcount>0:
-                        c += 1
-                c=math.log(self.N/float(c))
-                if c!=0:
-                    insertion_cursor.execute("INSERT INTO IDF(WORD,IDF_VALUE) VALUES('%s',%f)" % (current_word[0],c))
-        try:
-            self.db.commit()
-        except Exception, e:
-            self.db.rollback()
-            print e
-
 
     def addToDatabase(self, F, listOfWords, fid):
         print 'Indexing file %d' % fid
@@ -118,15 +124,22 @@ class Indexing:
         self.setUpTF(fid)
         for word in hashtable_words:
             self.TF_insert(fid, word, cnt, hashtable_words[word])
+        try:
+            self.db.commit()
+        except Exception, e:
+            self.db.rollback()
+            print e
+
+    def removeStopWords(self, wordlist):
+        return [x for x in wordlist if x not in self.stopWordSet]
 
     def fetchStemmedWords(self, path):
         with open(path, 'r') as F:
-            text = F.read()
-            text = text.lower()
+            text = F.read().lower()
             text = re.sub(r'[^a-z0-9 ]', ' ', text)
             listOfWords = text.split(' ')
             listOfWords = self.removeStopWords(listOfWords)
-            listOfWords = [stemmer.stem(word, 0, len(word) - 1) for word in listOfWords]
+            #listOfWords = [stemmer.stem(word, 0, len(word) - 1) for word in listOfWords]
         return listOfWords
 
     def parseFile(self, path_to_file, fid):
@@ -134,14 +147,28 @@ class Indexing:
         with open(path_to_file, 'r') as F:
             self.addToDatabase(F, listOfWords, fid)
 
+    def insertFile(self, id, f):
+        cur = self.db.cursor()
+        cur.execute(("INSERT INTO FILES(FILE_ID,LINK) VALUES(%d,'%s')" % (id, f)))
+
+
     def createIndex(self, pathtofolder):
         filesinside = []
+        self.setUpFileInfo()
+        ID = 0
         for f in os.listdir(pathtofolder):
             if os.path.isfile(os.path.join(pathtofolder, f)):
                 f = os.path.join(pathtofolder, f)
-                # print f
                 filesinside.append(f)
-        self.N = len(filesinside)
+                ID += 1
+                self.insertFile(ID, f)
+        try:
+            self.db.commit()
+        except Exception, e:
+            self.db.rollback()
+            print e
+
+        self.N = ID
         self.setUpIDF()
         self.getStopwords()
         for i in xrange(1, self.N + 1):
