5a4e5ea66bdbdeafc4b7d74dd8aa5b3f67899c9d
Shikhar-S <shikhar.coder@gmail.com>
2016-05-31 16:59:55 +0530

    Add files via upload

--- mainFile.py
+++ mainFile.py
@@ -1,5 +1,6 @@
 import Index_individual
 from Querying import Querying
+from Query_MMM import Query_MMM
 from timeit import default_timer as timer
 import os
 path=raw_input('Enter folder name:\n')
@@ -14,9 +15,19 @@ if flag=='Y':
     end=timer()
     print 'Time taken to index %d files = %f' % (indexer.N,end-start)
 
+###################################
 queryProcessor=Querying(path)
 queryProcessor.getQuery()
 start=timer()
 queryProcessor.processQuery()
 end=timer()
 print 'Time taken to search %f' % (end-start)
+####################################
+
+
+queryProcessor_MMM=Query_MMM(path)
+queryProcessor_MMM.getQuery()
+start=timer()
+queryProcessor_MMM.processQuery()
+end=timer()
+print 'Time taken by MMM algo %f' % (end-start)
