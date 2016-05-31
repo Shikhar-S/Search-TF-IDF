533640e4145a1a8956bb83c6690146e409e31d67
Shikhar-S <shikhar.coder@gmail.com>
2016-05-31 10:24:41 +0530

    Update mainFile.py

--- mainFile.py
+++ mainFile.py
@@ -1,5 +1,22 @@
-from Index_individual import Indexing
-path=raw_input('Enter file path:\n')
-stop=raw_input('Enter stopword file path:\n')
-indexer=Indexing(stop)
-indexer.createIndex(path)
+import Index_individual
+from Querying import Querying
+from timeit import default_timer as timer
+import os
+path=raw_input('Enter folder name:\n')
+path=os.path.join(os.getcwd(),path)
+stop=raw_input('Enter stopword file name:\n')
+stop=os.path.join(os.getcwd(),stop)
+flag=raw_input("Do indexing?? Y: Yes N:NO\n")
+if flag=='Y':
+    start=timer()
+    indexer=Index_individual.Indexing(stop)
+    indexer.createIndex(path)
+    end=timer()
+    print 'Time taken to index %d files = %f' % (indexer.N,end-start)
+
+queryProcessor=Querying(path)
+queryProcessor.getQuery()
+start=timer()
+queryProcessor.processQuery()
+end=timer()
+print 'Time taken to search %f' % (end-start)
