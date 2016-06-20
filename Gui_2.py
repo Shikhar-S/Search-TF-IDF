11e801a2c44740f4da2ff4a698a6b5e2cfc12471
Shikhar-S <shikhar.coder@gmail.com>
2016-06-20 14:12:51 +0530

    add support for Fuzzy Retrieval

--- Gui_2.py
+++ Gui_2.py
@@ -3,6 +3,7 @@ from Tkinter import *
 from ttk import *
 import webbrowser
 from Querying import Querying
+from Query_MMM import Query_MMM
 class Searcher:
     def __init__(self,parent,path,width="576",height="475"):
         self.path=path
@@ -12,6 +13,8 @@ class Searcher:
         self.window.title("File Searcher")
         self.srch_label=Label(self.window,text='Enter Search Query:')
         self.srch_box=Text(self.window,height=1,width=50)
+        self.label_algo=Label(self.window,text='Select Algorithm to Use')
+        self.choice_algo=StringVar()
         self.search_button=Button(self.window,command=self.start_search,text='Search')
         self.label_frame=None
         self.pack()
@@ -20,6 +23,8 @@ class Searcher:
     def pack(self):
         self.srch_label.pack(side="top",padx=5,pady=5)
         self.srch_box.pack(padx=5,pady=5)
+        Radiobutton(self.window, text='Vector Space Method', variable=self.choice_algo, value='VECTOR_SPACE').pack(padx=5,pady=5,anchor=W)
+        Radiobutton(self.window, text='Maxed min and max Model', variable=self.choice_algo, value='FUZZY_LOGIC').pack(padx=5,pady=5,anchor=W)
         self.search_button.pack(padx=5,pady=5)
 
 
@@ -32,17 +37,23 @@ class Searcher:
     def start_search(self):
         self.refreshresults()
         search_query = self.srch_box.get("1.0", END)
-        queryProcessor = Querying(path_to_folder=self.path,query=search_query)
-        start = timer()
-        result=queryProcessor.processQuery()
-        end = timer()
-        print 'Time taken to search %f' % (end - start)
+        if self.choice_algo.get()=='VECTOR_SPACE':
+            queryProcessor = Querying(path_to_folder=self.path, query=search_query)
+            start = timer()
+            result=queryProcessor.processQuery()
+            end = timer()
+            print 'Time taken to search %f' % (end - start)
+        else:
+            queryProcessor_MMM = Query_MMM(self.path,search_query)
+            start = timer()
+            result=queryProcessor_MMM.processQuery()
+            end = timer()
+            print 'Time taken by MMM algo %f' % (end - start)
         self.display(result)
 
     def open_in_browser(self,event):
         add="file://"+event.widget['text']
         webbrowser.open_new(add)
-
 
     def display(self,result):
         labels=[]
