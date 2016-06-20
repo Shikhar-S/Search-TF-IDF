dbc1b33d3560f72f9e74152f999213230d4e8f54
Shikhar-S <shikhar.coder@gmail.com>
2016-06-20 14:17:24 +0530

    Update mainFile.py

--- mainFile.py
+++ mainFile.py
@@ -1,3 +1,4 @@
+'''
 import Index_individual
 from Querying import Querying
 from Query_MMM import Query_MMM
@@ -8,26 +9,34 @@ path=os.path.join(os.getcwd(),path)
 stop=raw_input('Enter stopword file name:\n')
 stop=os.path.join(os.getcwd(),stop)
 flag=raw_input("Do indexing?? Y: Yes N:NO\n")
+indexer=Index_individual.Indexing(stop)
 if flag=='Y':
     start=timer()
-    indexer=Index_individual.Indexing(stop)
     indexer.createIndex(path)
     end=timer()
     print 'Time taken to index %d files = %f' % (indexer.N,end-start)
-
 ###################################
 queryProcessor=Querying(path)
 queryProcessor.getQuery()
 start=timer()
-queryProcessor.processQuery()
+result=queryProcessor.processQuery()
 end=timer()
+#for i in result:
+ #   print i
 print 'Time taken to search %f' % (end-start)
 ####################################
-
-
 queryProcessor_MMM=Query_MMM(path)
 queryProcessor_MMM.getQuery()
 start=timer()
 queryProcessor_MMM.processQuery()
 end=timer()
 print 'Time taken by MMM algo %f' % (end-start)
+'''
+
+from GUI import File_Loader
+from Gui_2 import Searcher
+from Tkinter import *
+root=Tk()
+root.iconify()
+FL=File_Loader(root)
+QS=Searcher(root,FL.dir_path)
